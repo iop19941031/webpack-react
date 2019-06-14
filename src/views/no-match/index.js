f884af05bcb69285a7ff6c61c85345532484aeb9
liang <402284347@qq.com>
2019-06-15 04:18:42 +0800

    404页面

---
+++ src/views/no-match/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+class NoMatch extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props)
+    }
+    render() {
+        return (
+            <div>
+                <h3>No match for <code>{this.props.location.pathname}</code></h3>
+            </div>
+        )
+    }
+}
+export default NoMatch
