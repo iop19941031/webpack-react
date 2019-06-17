5a5ec9bf300f52ea7dd357509e34fd64e823edd9
liang <402284347@qq.com>
2019-06-18 05:53:52 +0800

    编辑html的header

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import { Provider } from 'react-redux'
-
+import { Helmet } from 'react-helmet'
 import store from './store/stote-dools'
 import Home from './router/index'
 import App from './store/index'
 import index_css from './index.scss'
 ReactDOM.render(
-  <Provider store={store}>
-    <div className={index_css.a}>SCSS</div>
-    <Home />
-    <App />
-  </Provider>,
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My webpack</title>
+    </Helmet>
+    <Provider store={store}>
+      <div className={index_css.a}>SCSS</div>
+      <Home />
+      <App />
+    </Provider>
+  </div>,
   document.getElementById('root')
 )
