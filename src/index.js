a5215cec0092cb3d1518612ddefc27e6c12c7831
liangningning <liangningning@okaygis.com>
2020-12-08 17:21:10 +0800

    1.整理redux-action 2.还原redux和react-router demo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import 'babel-polyfill'
-import Home from "./view/home";
-// import { Provider } from 'react-redux'
-// import store from './store/stote-dools'
-// import Store from './store/index'
+import { Provider } from 'react-redux'
+import store from './store/stote-dools'
+import Store from './store/index'
 // import Router from './router/index'
-// import { setLinkConfig } from '@/router/links-config'
-// setLinkConfig()
+import { setLinkConfig } from '@/router/links-config'
+setLinkConfig()
 ReactDOM.render(
-  <Home />,
+  <div>
+    <Provider store={store}>
+      <Store />
+    </Provider>
+    {/* <Router /> */}
+  </div>,
   document.getElementById('root')
 )
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,13 +4,13 @@ const reducer = (state = State, action) => {
   switch (action.type) {
     case '涨工资':
       return {
+        ...state,
         tiger: state.tiger + 100,
-        ff: 1
       }
     case '扣工资':
       return {
+        ...state,
         tiger: state.tiger - 100,
-        ff: 2
       }
     default:
       return state
